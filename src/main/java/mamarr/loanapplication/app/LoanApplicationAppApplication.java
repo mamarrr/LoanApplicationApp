52fe46595110e291053088fb69eddee2c1c45544
Markus <mamarr@taltech.ee>
2026-06-15 15:53:24 +0300

    init

---
+++ src/main/java/mamarr/loanapplication/app/LoanApplicationAppApplication.java
@@ -0,0 +1,13 @@
+package mamarr.loanapplication.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationAppApplication.class, args);
+    }
+
+}
